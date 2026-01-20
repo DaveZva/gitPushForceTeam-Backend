857e3c09e149d162263d41e1f16d0fc726954ef2
david <david-varga@seznam.cz>
2026-01-21 00:48:05 +0100

    Edit user registration (without firstname and lastname)

--- src/main/java/com/gpfteam/catshow/catshow_backend/dto/RegisterRequest.java
+++ src/main/java/com/gpfteam/catshow/catshow_backend/dto/RegisterRequest.java
@@ -2,8 +2,6 @@ package com.gpfteam.catshow.catshow_backend.dto;
 import lombok.Data;
 @Data
 public class RegisterRequest {
-    private String firstName;
-    private String lastName;
     private String email;
     private String password;
 }
